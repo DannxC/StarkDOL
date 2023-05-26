394b51e6b2ff503c94111359efe33377ec71b923
Rafael450 <rafaelcamargo633@gmail.com>
2023-05-26 19:43:11 -0300

    feat: created basic transfer function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,45 @@
 import express from 'express';
+import { ethers } from 'ethers';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+
+const senderPrivateKey = process.env.SENDER_PRIVATE_KEY || '';
+const recipientAddress = '';
+const providerUrl = process.env.INFURA_URL || '';
+const usdcAddress = process.env.USDC_ADDRESS || '';
+
+// Connect to the network
+let provider = new ethers.JsonRpcProvider(providerUrl);
+
+// Create a wallet instance
+let wallet = new ethers.Wallet(senderPrivateKey, provider);
+
+app.get('/trigger-payment', async (req, res) => {
+  try {
+    // Connect to USDC contract on the network
+    const contract = new ethers.Contract(usdcAddress, [
+      "function transfer(address to, uint amount) public",
+    ], wallet);
+
+    // Amount to transfer (change this as necessary)
+    const amount = ethers.parseUnits("1.0", 6); // 1 USDC
+
+    // Transfer the USDC
+    const tx = await contract.transfer(recipientAddress, amount);
+
+    // Wait for the transaction to be mined
+    const receipt = await provider.waitForTransaction(tx.hash);
+    console.log(receipt);
+    res.send(`Transaction successful with hash: ${receipt}`);
+  } catch (error : any) {
+    res.send(`Error: ${error.message}`);
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
